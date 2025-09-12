5c9b9721120c3dd88b382995a8af8d2f0335aca4
AaronGutierrez <asmogub@outlook.com>
2025-09-12 16:23:05 -0500

    i did it but i had to find some info on internet

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -5,5 +5,42 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise4 Project.");
+
+        List<int> numbers = new List<int>();
+
+        int numberObtained = -1;
+
+        while (numberObtained != 0)
+        {
+            Console.Write("Enter a number: ");
+            numberObtained = int.Parse(Console.ReadLine());
+
+            if (numberObtained != 0)
+            {
+                numbers.Add(numberObtained);
+            }
+        }
+
+        int total = 0;
+        foreach (int number in numbers)
+        {
+            total += number;
+        }
+        Console.WriteLine($"The total is: {total}");
+
+        float average = ((float)total) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int max = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The max is: {max}");
+
+        
     }
 }
