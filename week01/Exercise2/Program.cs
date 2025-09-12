8c3fadb275f4655e44d5371a6a0bb8dd1ce8138b
AaronGutierrez <asmogub@outlook.com>
2025-09-12 13:08:02 -0500

    Done

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,30 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.Write("What is your percent grade? ");
+        string studentAnswer = Console.ReadLine();
+        int studentGrade = int.Parse(studentAnswer);
+
+        if (studentGrade >= 90)
+        {
+            Console.WriteLine("You got a A");
+        }
+        else if (studentGrade >= 80)
+        {
+            Console.WriteLine("You got a B");
+        }
+        else if (studentGrade >= 70)
+        {
+            Console.WriteLine("You got a C");
+        }
+        else if (studentGrade >= 60)
+        {
+            Console.WriteLine("You got a D");
+        }
+        else
+        {
+            Console.WriteLine("You got a F");
+        }        
     }
 }
