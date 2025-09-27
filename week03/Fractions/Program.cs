f0a36ffab2ccfa99af1819b5bd6168177763b6e3
AaronGutierrez <asmogub@outlook.com>
2025-09-26 19:01:33 -0500

    Practice finished

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -5,5 +5,18 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
+
+        Fractions f = new Fractions();
+        Console.WriteLine(f.GetFractionString());
+        Console.WriteLine(f.GetDecimalValue());
+
+        Fractions f2 = new Fractions(6);
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+        
+        Fractions f3 = new Fractions(6, 7);
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+        
     }
 }
